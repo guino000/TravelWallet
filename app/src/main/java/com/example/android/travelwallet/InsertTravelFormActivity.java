db187815b584e0a0bd679821814e91378347c26b
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-09-24 00:11:00 +0200

    Added datepicker dialog to Travel Insert Form

--- app/src/main/java/com/example/android/travelwallet/InsertTravelFormActivity.java
+++ app/src/main/java/com/example/android/travelwallet/InsertTravelFormActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.travelwallet;
 
+import android.app.DatePickerDialog;
 import android.arch.lifecycle.ViewModelProvider;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,6 +16,9 @@ import com.example.android.travelwallet.model.Travel;
 import com.example.android.travelwallet.model.TravelViewModel;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,11 +43,47 @@ public class InsertTravelFormActivity extends AppCompatActivity {
     @BindView(R.id.bt_create_travel)
     Button mAddTravelButton;
 
+    Calendar mCalendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_travel_form);
         ButterKnife.bind(this);
+
+        mCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                mCalendar.set(Calendar.YEAR, year);
+                mCalendar.set(Calendar.MONTH, month);
+                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            }
+        };
+
+        mStartDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(InsertTravelFormActivity.this, dateSetListener,
+                        mCalendar.get(Calendar.YEAR),
+                        mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+                updateLabel(mStartDateEditText);
+            }
+        });
+
+        mEndDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(InsertTravelFormActivity.this, dateSetListener,
+                        mCalendar.get(Calendar.YEAR),
+                        mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+                updateLabel(mEndDateEditText);
+            }
+        });
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -72,5 +115,11 @@ public class InsertTravelFormActivity extends AppCompatActivity {
                 throw  new UnsupportedOperationException();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateLabel(EditText dateEditText){
+        String format = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+        dateEditText.setText(sdf.format(mCalendar.getTime()));
     }
 }

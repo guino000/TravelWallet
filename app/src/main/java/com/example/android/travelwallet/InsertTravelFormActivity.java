ac51c0f79969957b8a9d3eade78ccbfd7143a0c3
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-09-23 21:57:44 +0200

    Created Main, Add Travel and Travel Miniature layouts

---
+++ app/src/main/java/com/example/android/travelwallet/InsertTravelFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.travelwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertTravelFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_travel_form);
+    }
+}

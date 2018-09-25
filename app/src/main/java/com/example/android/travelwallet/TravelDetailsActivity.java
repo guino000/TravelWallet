a2f22a631f43c037881c43b4a631dea069bbae69
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-09-26 00:01:50 +0200

    Created layout for expense details activity and expense miniature

---
+++ app/src/main/java/com/example/android/travelwallet/TravelDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.travelwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TravelDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_details);
+    }
+}

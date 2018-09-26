db2991a622929ba182d3b3a48eef4f5312c12ef2
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-09-26 23:38:46 +0200

    Added currency formatting to main activity budget and expense comparison.

--- app/src/main/java/com/example/android/travelwallet/utils/TravelUtils.java
+++ app/src/main/java/com/example/android/travelwallet/utils/TravelUtils.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.example.android.travelwallet.model.ExpenseViewModel;
 import com.example.android.travelwallet.model.Travel;
 import com.example.android.travelwallet.model.TravelExpense;
+import com.example.android.travelwallet.model.TravelValues;
 import com.example.android.travelwallet.model.TravelViewModel;
 
 import java.math.BigDecimal;
@@ -18,18 +19,15 @@ import java.util.Locale;
 
 public abstract class TravelUtils {
     public static String getBudgetExpenseComparison(Application application, Travel travel){
-        BigDecimal expenseTotal = new BigDecimal(0);
         ExpenseViewModel expenseViewModel = ViewModelProvider.AndroidViewModelFactory
                 .getInstance(application).create(ExpenseViewModel.class);
-        LiveData<List<TravelExpense>> expenses = expenseViewModel.getAllExpensesOfTravel(travel.getId());
+        TravelValues expenses = expenseViewModel.getTotalExpensesOfTravel(travel.getId());
 
-        if(expenses.getValue() == null) return "";
+        if(expenses == null) return "";
 
-        for (TravelExpense expense : expenses.getValue()){
-            expenseTotal = expenseTotal.add(expense.getExpenseTotal());
-        }
-
-        return String.format("%s - %s", expenseTotal.toString(), travel.getBudget().toString());
+        return String.format("%s - %s",
+                getCurrencyFormattedValue(new BigDecimal(expenses.total)),
+                getCurrencyFormattedValue(travel.getBudget()));
     }
 
     public static String getCurrencyFormattedValue(BigDecimal value){
